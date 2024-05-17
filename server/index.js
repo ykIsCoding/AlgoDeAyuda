64b88f4286a9b48f09f0caf6dccadaa999c7adab
ykIsCoding <ylim189@e.ntu.edu.sg>
2024-05-17 23:31:46 +0800

    updated backend

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config()
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const bodyParser = require('body-parser');
+const oAiRoutes = require('../routes/oAI')
+const gAiRoutes = require('../routes/gAI')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use('/oai',oAiRoutes)
+app.use('/gai',gAiRoutes)
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+  
+
+var corsOptions = {
+  origin: "http://localhost:4200",
+  credentials: true,
+  optionsSuccessStatus: 200,
+  headers: {
+    "Access-Control-Allow-Origin": "http://localhost:4200",
+    "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS", 
+    "Access-Control-Allow-Headers": "*", 
+  },
+};
+
+
+
+app.listen(3000||process.env.PORT)
