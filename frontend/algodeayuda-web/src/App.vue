a04fecf0ec1eb03e05bc090c9ec2ab381b3c6fcf
ykIsCoding <ylim189@e.ntu.edu.sg>
2024-05-28 12:32:58 +0800

    added speed-insights

--- frontend/algodeayuda-web/src/App.vue
+++ frontend/algodeayuda-web/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app class="w-screen h-100 pa-0 ma-0">
+    <SpeedInsights/>
     <v-main class="h-100 pa-0 ma-0">
       <RouterView />
     </v-main>
@@ -9,6 +10,6 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import SideBar from '@/components/SideBar.vue'
-  
+  import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
